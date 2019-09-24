4fa82d05910287650496696a354d2e10e1b173a1
st52558 <st52558@student.upce.cz>
2019-09-24 13:24:22 +0200

    cviko

---
+++ cv01/cv01/MagicBox.cpp
@@ -0,0 +1,38 @@
+#include "MagicBox.h"
+
+namespace MagicBoxLibrary{
+	MagicBox::MagicBox()
+	{
+
+	}
+
+	void MagicBox::Call() const
+	{
+
+	}
+}
+
+// nebo
+
+using namespace MagicBoxLibrary;
+MagicBox::MagicBox()
+{
+
+}
+
+void MagicBox::Call() const
+{
+
+}
+
+// nebo
+
+MagicBoxLibrary::MagicBox::MagicBox()
+{
+
+}
+
+void MagicBoxLibrary::MagicBox::Call() const
+{
+
+}
---
+++ cv01/cv01/MagicBox.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MAGIC_BOX_H
+#define MAGIC_BOX_H
+
+
+namespace MagicBoxLibrary{
+class MagicBox
+{
+public:
+	MagicBox();
+	void Call() const;
+};
+}
+#endif // MAGIC_BOX_H
