07c5aeb928990e81064a09447cb25c4f5e9a2f73
st52558 <st52558@student.upce.cz>
2019-11-25 16:51:09 +0100

    cv06 txt done

--- cv06DataStreams/DataStreams/DataStreams/DataStreams.cpp
+++ cv06DataStreams/DataStreams/DataStreams/DataStreams.cpp
@@ -1,15 +1,102 @@
-#include "Date.h"
-#include "Address.h"
 #include "Person.h"
 #include <fstream>
+#include <sstream>
+
+void writeToConsole(Person** persons, int count) {
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << *persons[i] << std::endl;
+	}
+}
+
+void load() {
+	Person** persons = nullptr;
+	std::string line;
+	int personsCount = 0;
+
+	std::ifstream in{};
+	in.open("././output.txt");
+
+	while (getline(in, line)) {
+		personsCount++;
+	}
+
+	in.clear();
+	in.seekg(0);
+
+	persons = new Person*[personsCount];
+
+	std::string surname;
+	std::string name;
+	int day;
+	int month;
+	int year;
+	std::string street;
+	std::string city;
+	int zipCode;
+
+	int counter = 0;
+	char delimiter = ',';
+	char delimiterDot = '.';
+	char delimiterSpacebar = ' ';
+	std::string token;
+
+	while (getline(in, line)) {
+		std::stringstream ss(line);
+		getline(ss, name, delimiterSpacebar);
+		getline(ss, surname, delimiter);
+		getline(ss, token, delimiterDot);
+		day = stoi(token);
+		getline(ss, token, delimiterDot);
+		month = stoi(token);
+		getline(ss, token, delimiter);
+		year = stoi(token);
+		getline(ss, street, delimiter);
+		getline(ss, city, delimiter);
+		getline(ss, token, '\n');
+		zipCode = stoi(token);
+
+		persons[counter] = new Person(name, surname, new Address(street, city, zipCode), new Date(day, month, year));
+		counter++;
+	}
+	in.close();
+	writeToConsole(persons, personsCount);
+
+	for (int i = 0; i < 3; i++)
+	{
+			delete persons[i];
+	}
+
+	delete[] persons;
+	}
+
+void save() {
+	Person** persons = new Person * [3];
+
+	persons[0] = new Person("Petr", "Cerveny", new Address("Zelena", "Praha", 10000), new Date(21, 5, 1984));
+	persons[1] = new Person("Pavel", "Zluty", new Address("Bila", "Brno", 20000), new Date(2, 4, 1991));
+	persons[2] = new Person("Roman", "Cerny", new Address("Cervena", "Pardubice", 33333), new Date(16, 10, 1996));
+
+	std::ofstream out{};
+	out.open("././output.txt");
+
+	for (int i = 0; i < 3; i++)
+	{
+		out << *persons[i] << std::endl;
+	}
+	out.close();
+
+
+	for (int i = 0; i < 3; i++)
+	{
+		delete persons[i];
+	}
+	delete[] persons;
+}
 
 int main(int agrc, char** argv)
 {
-	std::ofstream outputFile{ "././output.txt" };
-	outputFile << "Prvni retezec 1234567890 123" << std::endl;
-	Person person1 = Person();
-	"Petr Cerveny, 1. 2. 2011, Zelena, Praha, 10000" >> person1;
-	Person person2 = Person();
-	std::string cin = "Pavel Zluty, 3. 4. 2055, Cerna, Brno, 20000";
-	cin >> person2;
+	save();
+	load();
 }
+
--- cv06DataStreams/DataStreams/DataStreams/Person.h
+++ cv06DataStreams/DataStreams/DataStreams/Person.h
@@ -6,20 +6,25 @@
 
 struct Person
 {
-public:
+private:
 	std::string _name;
 	std::string _surname;
-	Address _address;
-	Date _dateOfBirth;
-
+	Address* _address;
+	Date* _dateOfBirth;
+public:
+	Person(std::string name, std::string surname, Address* address, Date* date) : _name(name), _surname(surname), _address(address), _dateOfBirth(date) {};
 	std::string getName() const { return _name; }
 	std::string getSurname() const { return _surname; }
+	Address getAddress() const { return *_address; }
+	Date getDate() const { return *_dateOfBirth; }
 	void setName(std::string name) { _name = name; }
 	void setSurname(std::string surname) { _surname = surname; }
+	void setAddress(Address* address) { _address = address; }
+	void setDate(Date* date) { _dateOfBirth = date; }
 };
 
 std::ostream& operator<<(std::ostream& os, const Person& person) {
-	os << person.getName() << person.getSurname() << ", " << person._dateOfBirth << person._address;
+	os << person.getName() << " " << person.getSurname() << ", " << person.getDate() << person.getAddress();
 	return os;
 }
 std::istream& operator>>(std::istream& is, Person& person) {
@@ -33,11 +38,11 @@ std::istream& operator>>(std::istream& is, Person& person) {
 
 	Date date;
 	is >> date;
-	person._dateOfBirth = date;
+	person.setDate(&date);
 
 	Address address;
 	is >> address;
-	person._address = address;
+	person.setAddress(&address);
 
 	return is;
 }
